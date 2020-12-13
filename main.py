fca2c17d0faa00bb7ea0afd27a8eb2f4c2b41276
HelmiHalmouch <helmirealmadrid@gmail.com>
2020-12-13 22:47:49 +0100

    reform code style

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 """
-CNN for image classification 
+CNN for image classification
 
-GHNAMI Helmi 
-and 
-GHANMI Massaoud 
+GHNAMI Helmi
+and
+GHANMI Massaoud
 """
 
 # Import libraries
-import os,cv2
+from keras.optimizers import SGD, RMSprop, adam
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.models import Sequential
+from keras.utils import np_utils, plot_model
+import os
+import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -17,11 +23,6 @@ from sklearn.model_selection import train_test_split
 
 from keras import backend as K
 K.set_image_dim_ordering('th')
-from keras.utils import np_utils, plot_model
-from keras.models import Sequential
-from keras.layers.core import Dense, Dropout, Activation, Flatten
-from keras.layers.convolutional import Conv2D, MaxPooling2D
-from keras.optimizers import SGD,RMSprop,adam
 
 
 #------------------Preprocessing of input datasets using sklearn-----------------#
@@ -32,123 +33,127 @@ PATH = os.getcwd()
 data_path = PATH + '/data'
 data_dir_list = os.listdir(data_path)
 
-img_rows=128
-img_cols=128
-num_channel=1
+img_rows = 128
+img_cols = 128
+num_channel = 1
 
-img_data_list=[]
+img_data_list = []
 
 for dataset in data_dir_list:
-	img_list=os.listdir(data_path+'/'+ dataset)
-	print ('Loaded the images of dataset-'+'{}\n'.format(dataset))
-	for img in img_list:
-		input_img=cv2.imread(data_path + '/'+ dataset + '/'+ img )
-		input_img=cv2.cvtColor(input_img, cv2.COLOR_BGR2GRAY)
-		input_img_resize=cv2.resize(input_img,(128,128))
-		img_data_list.append(input_img_resize)
+    img_list = os.listdir(data_path+'/' + dataset)
+    print('Loaded the images of dataset-'+'{}\n'.format(dataset))
+    for img in img_list:
+        input_img = cv2.imread(data_path + '/' + dataset + '/' + img)
+        input_img = cv2.cvtColor(input_img, cv2.COLOR_BGR2GRAY)
+        input_img_resize = cv2.resize(input_img, (128, 128))
+        img_data_list.append(input_img_resize)
 
 img_data = np.array(img_data_list)
 img_data = img_data.astype('float32')
 img_data /= 255
-print (img_data.shape)
+print(img_data.shape)
 
-if num_channel==1:
-	if K.image_dim_ordering()=='th':
-		img_data= np.expand_dims(img_data, axis=1) 
-		print (img_data.shape)
-	else:
-		img_data= np.expand_dims(img_data, axis=4) 
-		print (img_data.shape)
-		
+if num_channel == 1:
+    if K.image_dim_ordering() == 'th':
+        img_data = np.expand_dims(img_data, axis=1)
+        print(img_data.shape)
+    else:
+        img_data = np.expand_dims(img_data, axis=4)
+        print(img_data.shape)
+
 else:
-	if K.image_dim_ordering()=='th':
-		img_data=np.rollaxis(img_data,3,1)
-		print (img_data.shape)
-		
-#%%
-USE_SKLEARN_PREPROCESSING=False
+    if K.image_dim_ordering() == 'th':
+        img_data = np.rollaxis(img_data, 3, 1)
+        print(img_data.shape)
+
+# %%
+USE_SKLEARN_PREPROCESSING = False
 
 if USE_SKLEARN_PREPROCESSING:
-	# using sklearn for preprocessing
-	from sklearn import preprocessing
-	
-	def image_to_feature_vector(image, size=(128, 128)):
-		# resize the image to a fixed size, then flatten the image into
-		# a list of raw pixel intensities
-		return cv2.resize(image, size).flatten()
-	
-	img_data_list=[]
-	for dataset in data_dir_list:
-		img_list=os.listdir(data_path+'/'+ dataset)
-		print ('Loaded the images of dataset-'+'{}\n'.format(dataset))
-		for img in img_list:
-			input_img=cv2.imread(data_path + '/'+ dataset + '/'+ img )
-			input_img=cv2.cvtColor(input_img, cv2.COLOR_BGR2GRAY)
-			input_img_flatten=image_to_feature_vector(input_img,(128,128))
-			img_data_list.append(input_img_flatten)
-	
-	img_data = np.array(img_data_list)
-	img_data = img_data.astype('float32')
-	print (img_data.shape)
-	img_data_scaled = preprocessing.scale(img_data)
-	print (img_data_scaled.shape)
-	
-	print (np.mean(img_data_scaled))
-	print (np.std(img_data_scaled))
-	
-	print (img_data_scaled.mean(axis=0))
-	print (img_data_scaled.std(axis=0))
-	
-	if K.image_dim_ordering()=='th':
-		img_data_scaled=img_data_scaled.reshape(img_data.shape[0],num_channel,img_rows,img_cols)
-		print (img_data_scaled.shape)
-		
-	else:
-		img_data_scaled=img_data_scaled.reshape(img_data.shape[0],img_rows,img_cols,num_channel)
-		print (img_data_scaled.shape)
-	
-	
-	if K.image_dim_ordering()=='th':
-		img_data_scaled=img_data_scaled.reshape(img_data.shape[0],num_channel,img_rows,img_cols)
-		print (img_data_scaled.shape)
-		
-	else:
-		img_data_scaled=img_data_scaled.reshape(img_data.shape[0],img_rows,img_cols,num_channel)
-		print (img_data_scaled.shape)
+    # using sklearn for preprocessing
+    from sklearn import preprocessing
+
+    def image_to_feature_vector(image, size=(128, 128)):
+        # resize the image to a fixed size, then flatten the image into
+        # a list of raw pixel intensities
+        return cv2.resize(image, size).flatten()
+
+    img_data_list = []
+    for dataset in data_dir_list:
+        img_list = os.listdir(data_path+'/' + dataset)
+        print('Loaded the images of dataset-'+'{}\n'.format(dataset))
+        for img in img_list:
+            input_img = cv2.imread(data_path + '/' + dataset + '/' + img)
+            input_img = cv2.cvtColor(input_img, cv2.COLOR_BGR2GRAY)
+            input_img_flatten = image_to_feature_vector(input_img, (128, 128))
+            img_data_list.append(input_img_flatten)
+
+    img_data = np.array(img_data_list)
+    img_data = img_data.astype('float32')
+    print(img_data.shape)
+    img_data_scaled = preprocessing.scale(img_data)
+    print(img_data_scaled.shape)
+
+    print(np.mean(img_data_scaled))
+    print(np.std(img_data_scaled))
+
+    print(img_data_scaled.mean(axis=0))
+    print(img_data_scaled.std(axis=0))
+
+    if K.image_dim_ordering() == 'th':
+        img_data_scaled = img_data_scaled.reshape(
+            img_data.shape[0], num_channel, img_rows, img_cols)
+        print(img_data_scaled.shape)
+
+    else:
+        img_data_scaled = img_data_scaled.reshape(
+            img_data.shape[0], img_rows, img_cols, num_channel)
+        print(img_data_scaled.shape)
+
+    if K.image_dim_ordering() == 'th':
+        img_data_scaled = img_data_scaled.reshape(
+            img_data.shape[0], num_channel, img_rows, img_cols)
+        print(img_data_scaled.shape)
+
+    else:
+        img_data_scaled = img_data_scaled.reshape(
+            img_data.shape[0], img_rows, img_cols, num_channel)
+        print(img_data_scaled.shape)
 
 if USE_SKLEARN_PREPROCESSING:
-	img_data=img_data_scaled
+    img_data = img_data_scaled
 
 #--------------------Split the dataset into train adn test and define the labels ------------------#
 # Define the number of classes
 num_classes = 4
 
 num_of_samples = img_data.shape[0]
-labels = np.ones((num_of_samples,),dtype='int64')
+labels = np.ones((num_of_samples,), dtype='int64')
 
-labels[0:202]=0
-labels[202:404]=1
-labels[404:606]=2
-labels[606:]=3
+labels[0:202] = 0
+labels[202:404] = 1
+labels[404:606] = 2
+labels[606:] = 3
 
-names = ['cats','dogs','horses','humans']
-	  
+names = ['cats', 'dogs', 'horses', 'humans']
+
 # convert class labels to on-hot encoding
 Y = np_utils.to_categorical(labels, num_classes)
 
-#Shuffle the dataset
-x,y = shuffle(img_data,Y, random_state=2)
+# Shuffle the dataset
+x, y = shuffle(img_data, Y, random_state=2)
 
-# Split the dataset : test_size = 0.2 ==> 20% of the image dataset are for the test and 80% for the train 
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=2)
+# Split the dataset : test_size = 0.2 ==> 20% of the image dataset are for the test and 80% for the train
+X_train, X_test, y_train, y_test = train_test_split(
+    x, y, test_size=0.2, random_state=2)
 
 #--------------Architechture of the sequential model---------------------#
 # Defining the model
-input_shape=img_data[0].shape
-					
+input_shape = img_data[0].shape
+
 model = Sequential()
 
-model.add(Conv2D(32, (3,3),border_mode='same',input_shape=input_shape))
+model.add(Conv2D(32, (3, 3), border_mode='same', input_shape=input_shape))
 model.add(Activation('relu'))
 model.add(Conv2D(32, (3, 3)))
 model.add(Activation('relu'))
@@ -158,7 +163,7 @@ model.add(Dropout(0.5))
 model.add(Conv2D(64, (3, 3)))
 model.add(Activation('relu'))
 #model.add(Conv2D(64, (3, 3)))
-#model.add(Activation('relu'))
+# model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.5))
 
@@ -173,20 +178,22 @@ model.add(Activation('softmax'))
 model.summary()
 model.get_config()
 model.layers[0].get_config()
-model.layers[0].input_shape			
-model.layers[0].output_shape			
+model.layers[0].input_shape
+model.layers[0].output_shape
 model.layers[0].get_weights()
 np.shape(model.layers[0].get_weights()[0])
 model.layers[0].trainable
 
 #------------------Compile train then predict the model -----------------#
 
-model.compile(loss='categorical_crossentropy', optimizer='rmsprop',metrics=["accuracy"])
+model.compile(loss='categorical_crossentropy',
+              optimizer='rmsprop', metrics=["accuracy"])
 # Training
-num_epoch=50
-cnn = model.fit(X_train, y_train, batch_size=16, epochs=num_epoch, verbose=1, validation_data=(X_test, y_test))
+num_epoch = 50
+cnn = model.fit(X_train, y_train, batch_size=16, epochs=num_epoch,
+                verbose=1, validation_data=(X_test, y_test))
 
-#preduction scores 
+# preduction scores
 scores = model.evaluate(X_test, y_test, verbose=0)
 print("Accuracy: %.2f%%" % (scores[1]*100))
 
@@ -197,28 +204,28 @@ with open("model.json", "w") as json_file:
     json_file.write(model_json)
 # serialize weights to HDF5
 model.save_weights("model.h5")
-print("Saved model to disk") 
+print("Saved model to disk")
 
 #----------------Plot the training  validation and loss accuracy ---------------#
 plt.figure(0)
-plt.plot(cnn.history['acc'],'r')
-plt.plot(cnn.history['val_acc'],'g')
+plt.plot(cnn.history['acc'], 'r')
+plt.plot(cnn.history['val_acc'], 'g')
 plt.xticks(np.arange(0, 11, 2.0))
 plt.rcParams['figure.figsize'] = (8, 6)
 plt.xlabel("Num of Epochs")
 plt.ylabel("Accuracy")
 plt.title("Training Accuracy vs Validation Accuracy")
-plt.legend(['train','validation'])
+plt.legend(['train', 'validation'])
 plt.savefig('train_validation_accray.png')
 
 plt.figure(1)
-plt.plot(cnn.history['loss'],'r')
-plt.plot(cnn.history['val_loss'],'g')
+plt.plot(cnn.history['loss'], 'r')
+plt.plot(cnn.history['val_loss'], 'g')
 plt.xticks(np.arange(0, 11, 2.0))
 plt.rcParams['figure.figsize'] = (8, 6)
 plt.xlabel("Num of Epochs")
 plt.ylabel("Loss")
 plt.title("Training Loss vs Validation Loss")
-plt.legend(['train','validation'])
+plt.legend(['train', 'validation'])
 
 plt.savefig('train_validation_loss.png')
